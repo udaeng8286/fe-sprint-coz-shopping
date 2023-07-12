f21b4db2f83186aa32ce63b87b6ec9ed7cfd40c2
udaeng8286 <udaenglvup@gmail.com>
2023-07-13 05:31:38 +0900

    feat: footer 구현 [#2]

---
+++ shopping/src/component/footer.js
@@ -0,0 +1,13 @@
+import './footer.css';
+
+function Footer() {
+    return (
+      <footer className="footer-container">
+        <div className="line-one">개인정보 처리방침 | 이용 약관</div>
+        <div className="line-two">All rigths reserved @ Codestates</div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
